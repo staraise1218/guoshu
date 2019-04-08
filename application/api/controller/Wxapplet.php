15d23b86b9aff9369b90f48f74bc1297618de762
shengli <363912809@qq.com>
2019-04-08 15:53:21 +0800

    wxapplet

--- application/api/controller/Wxapplet.php
+++ application/api/controller/Wxapplet.php
@@ -1,66 +1,45 @@
 <?php
 
 namespace app\api\controller;
+
 use think\Db;
-use app\api\logic\WxpayLogic;
 
-class Wxapplet extends Base {
+require_once "./plugins/payment/wxapplet/lib/WxPay.Api.php";
+require_once "./plugins/payment/wxapplet/WxPay.JsApiPay.php";
+require_once "./plugins/payment/wxapplet/WxPay.Config.php";
 
-	public function __construct(){
-		// 设置所有方法的默认请求方式
-		$this->method = 'POST';
+class Wxapplet {
 
-		parent::__construct();
-	}
+    // 微信jsapi 支付
+	// 统一下单 unifiedOrder
+    public function unifiedOrder(){
+        $order_sn = I('order_sn');
+        $openId = I('openid');
 
-
-	// 选择支付方式去支付
-	public function topay(){
-		$order_sn = I('order_sn');
-		$paymentMethod = I('paymentMethod');
-
-		/********* 判断订单信息 **************/
+        /********* 判断订单信息 **************/
 		$order = Db::name('order')->where('order_sn', $order_sn)->find();
 		if(empty($order)) response_error('', '该订单不存在');
 		if($order['paystatus'] == 1) response_error('', '该订单已支付');
 
 		$total_amount = $order['price'];
 
-		/************** 获取订单签名字符串 **************/
-		if($paymentMethod == 'alipay'){
-			$notify_url = 'http://guoshu.staraise.com.cn/index.php/api/goldcoin/alipayCallback';
-			$AlipayLogic = new AlipayLogic($notify_url);
-			$orderStr = $AlipayLogic->generateOrderStr($order_sn, $total_amount, '购买商品', '购买商品');
-			return $orderStr;
-		}
+    	//②、统一下单
+        $input = new \WxPayUnifiedOrder();
+        $input->SetBody("视频课程");
+        $input->SetAttach("视频课程");
+        $input->SetOut_trade_no($order_sn);
+        $input->SetTotal_fee($order['price']*100);
+        $input->SetTime_start(date("YmdHis"));
+        $input->SetTime_expire(date("YmdHis", time() + 600));
+        $input->SetGoods_tag("视频课程");
+        $input->SetNotify_url("http://guoshu.staraise.com.cn/api/WxappletBuyGoodsCallback/exec");
+        $input->SetTrade_type("JSAPI");
+        $input->SetOpenid($openId);
+        $config = new \WxPayConfig();
+        $order = \WxPayApi::unifiedOrder($config, $input);
 
-		if($paymentMethod == 'wxpay'){
-			$WxpayLogic = new WxpayLogic();
-			$WxpayLogic->notify_url = 'http://guoshu.staraise.com.cn/index.php/api/goldcoin/wxpayCallback';
-			$param = $WxpayLogic->getPrepayId($order_sn, $total_amount, '购买商品');
-			response_success($param);
-		}
-		// 余额支付
-		if($paymentMethod == 'money'){
-			
-		}
-	}
-
-	public function operation($order_sn){
-		// 启动事务
-		Db::startTrans();
-		try{
-			
-			// 更改订单状态
-			M('order')->where('order_sn', $order_sn)->update(array('paystatus'=>1, 'paytime'=>time()));
-		   
-
-		    // 提交事务
-		    Db::commit();
-
-		} catch (\Exception $e) {
-		    // 回滚事务
-		    Db::rollback();
-		}
-	}
+        $JsApiPay = new \JsApiPay();
+        $jsApiParameters = $JsApiPay->GetJsApiParameters($order);
+        echo $jsApiParameters;
+    }
 }
