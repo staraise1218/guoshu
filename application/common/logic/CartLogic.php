265e739d005f700e05a6a58c2651e27dfe28d90c
shengli <363912809@qq.com>
2019-04-30 15:24:26 +0800

    bug

--- application/common/logic/CartLogic.php
+++ application/common/logic/CartLogic.php
@@ -575,13 +575,11 @@ class CartLogic extends Model
         if($selected != 0){
             $cartWhere['selected'] = 1;
         }
-        $cartList = $cart->field('id, goods_num, goods_name, goods_sn,sku, goods_price, member_goods_price, selected,prom_type, goods_id')
+        $cartList = $cart->field('id, goods_num, goods_name, goods_sn,sku, goods_price, member_goods_price, selected,prom_type,prom_id, goods_id')
                         ->with(['promGoods', 'goods'=>function($query){
                             $query->field('goods_id, goods_name, subtitle, tag, shop_price, original_img, is_on_sale, cat_id, city_code');
                         }])
                         ->where($cartWhere)->select();  // 获取购物车商品
-        p($cartList);
-                        
         $cartCheckAfterList = $this->checkCartList($cartList);
 //        $cartCheckAfterList = $cartList;
         $cartGoodsTotalNum = array_sum(array_map(function($val){return $val['goods_num'];}, $cartCheckAfterList));//购物车购买的商品总数
