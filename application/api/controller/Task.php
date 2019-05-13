e43c79867a087e7c29daeb4a82e85d45155836c5
shengli <363912809@qq.com>
2019-05-13 18:31:44 +0800

    task

--- application/api/controller/Task.php
+++ application/api/controller/Task.php
@@ -41,4 +41,39 @@ class Task extends Base {
         Db::name('group_buy')->where('id', array('IN', $group_ids))->setField('is_end', 1);
         Db::name('goods')->where('goods_id', array('IN', $goods_ids))->update(array('prom_type'=>0, 'prom_id'=>0));
     }
+
+    // 订单超过24小时没支付就取消
+    public function checkOrder(){
+
+        $time = time() - (24 * 60 * 60);
+        $list = Db::name('order_goods')->alias('og')
+            ->join('order o', 'og.order_id = o.order_id')
+            ->where('o.pay_status', 0)
+            ->where('o.order_status', array('IN', array(0, 1)))
+            ->where('o.add_time', array('<', $time))
+            ->field('og.order_id, og.goods_id, og.goods_num')
+            ->select();
+
+        if(empty($list)) return;
+
+        // 启动事务
+        Db::startTrans();
+        try{
+            // 取消订单
+            $order_ids = array_unique(array_column($list, 'order_id'));
+            Db::name('order')->where('order_id', array('IN', $order_ids))->setField('order_status', 3);
+
+            foreach ($list as $item) {
+                // 商品库存返回
+                Db::name('goods')->where('goods_id', $item['goods_id'])->setInc('store_count', $item['goods_num']);
+            }
+
+            // 提交事务
+            Db::commit();
+
+        } catch (\Exception $e) {
+            // 回滚事务
+            Db::rollback();
+        }
+    }
 }
