444418ef93ee6dbf4fe42bd9b33c515d3c34df1f
shengli <363912809@qq.com>
2019-05-21 12:26:11 +0800

    tag

--- application/common/logic/CartLogic.php
+++ application/common/logic/CartLogic.php
@@ -575,7 +575,7 @@ class CartLogic extends Model
         if($selected != 0){
             $cartWhere['selected'] = 1;
         }
-        $cartList = $cart->field('id, goods_num, goods_name, goods_sn,sku, goods_price, member_goods_price, selected,prom_type,prom_id, goods_id')
+        $cartList = $cart->field('id, goods_id, goods_num, goods_name, goods_sn,sku, goods_price, member_goods_price, selected,prom_type,prom_id, tag')
                         ->with(['promGoods', 'goods'=>function($query){
                             $query->field('goods_id, goods_name, subtitle, tag, shop_price, original_img, is_on_sale, cat_id, city_code,prom_type,prom_id');
                         }])
