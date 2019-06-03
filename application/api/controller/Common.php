827e6536296193b45780168536d510cb4a798dfb
shengli <363912809@qq.com>
2019-06-03 10:30:24 +0800

    file

--- application/api/controller/Common.php
+++ application/api/controller/Common.php
@@ -55,25 +55,17 @@ class Common extends Base {
     public function uploadFile(){
         $type = I('type');
 
-        if( ! in_array($type, array('invite_image', 'dynamic_image', 'dynamic_video', 'head_pic'))) response_error('', '不被允许的类型');
+        if( ! in_array($type, array('goods_comment'))) response_error('', '不被允许的类型');
         if(empty($_FILES)) response_error('文件不能为空');
 
         /************* 上传路径 ***************/        
-        $uploadPath = UPLOAD_PATH.'files';
-        if($type == 'invite_image') $uploadPath = UPLOAD_PATH.'invite/image';
-        if($type == 'dynamic_image') $uploadPath = UPLOAD_PATH.'dynamics/image';
-        if($type == 'dynamic_video') $uploadPath = UPLOAD_PATH.'dynamics/video';
-        if($type == 'head_pic') $uploadPath = UPLOAD_PATH.'head_pic';
+        if($type == 'goods_comment') $uploadPath = UPLOAD_PATH.'goods_comment';
         
         $FileLogic = new FileLogic();
         $result = $FileLogic->uploadSingleFile('file', $uploadPath);
         if($result['status'] == '1'){
             $filepath = $result['fullPath'];
-            $thumb = '';
-            if($type == 'dynamic_video'){
-                $thumb = $FileLogic->video2thumb($filepath);
-            }
-            response_success(array('filepath'=>$filepath, 'thumb'=>$thumb));
+            response_success(array('filepath'=>$filepath));
         } else {
             response_error('', '文件上传失败');
         }
